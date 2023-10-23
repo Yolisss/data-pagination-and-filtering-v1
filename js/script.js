92ce221cfb84ebff394c812f4ce2b9d2b3fa5ec1
Yolis <yolisisabel28@gmail.com>
2023-10-23 14:53:18 -0700

    rendered student info to browser successfully

--- js/script.js
+++ js/script.js
@@ -19,6 +19,37 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 
+function showPage(list, page){
+   let itemsPerPage = 9;
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   
+   const listStudents = document.querySelector('.student-list');
+   listStudents.innerHTML = '';
+
+   for(let i = 0; i < list.length; i++){
+      //9 & 18
+      if(i >= startIndex && i < endIndex){
+        let html = `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">${list[i].registered.date}</span>
+            </div>
+         </li>`
+         listStudents.insertAdjacentHTML('beforeend', html);
+      }
+   }
+}
+
+showPage(data, 2);
+console.log(data);
+
+
+
 
 /*
 Create the `addPagination` function
